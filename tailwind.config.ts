42a11e2e0a88d35965b20dd71289530c8026871e
“N3M351” <gmtgeovane@gmail.com>
2024-05-06 01:51:18 -0300

    radix menu dropdown

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
-import { blackA } from '@radix-ui/colors'
+import { blackA, mauve, violet } from '@radix-ui/colors'
 /** @type {import('tailwindcss').Config} */
 
 const config: Config = {
@@ -34,6 +34,36 @@ const config: Config = {
               // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/color-mix
               glow: 'color-mix(in srgb, var(--glow-color) calc(<alpha-value> * 100%), transparent)',
               ...blackA,
+              ...mauve,
+              ...violet,
+            },
+            keyframes: {
+              slideDownAndFade: {
+                from: { opacity: '0', transform: 'translateY(-2px)' },
+                to: { opacity: '1', transform: 'translateY(0)' },
+              },
+              slideLeftAndFade: {
+                from: { opacity: '0', transform: 'translateX(2px)' },
+                to: { opacity: '1', transform: 'translateX(0)' },
+              },
+              slideUpAndFade: {
+                from: { opacity: '0', transform: 'translateY(2px)' },
+                to: { opacity: '1', transform: 'translateY(0)' },
+              },
+              slideRightAndFade: {
+                from: { opacity: '0', transform: 'translateX(-2px)' },
+                to: { opacity: '1', transform: 'translateX(0)' },
+              },
+            },
+            animation: {
+              slideDownAndFade:
+                'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+              slideLeftAndFade:
+                'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+              slideUpAndFade:
+                'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+              slideRightAndFade:
+                'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
             },
           },
         },
